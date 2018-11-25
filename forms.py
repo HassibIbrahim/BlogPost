7361096005a3ccd1ffd10fdc69e941457d654dbb
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-25 22:14:48 +0300

    created login form for signing in

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Required,DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -9,4 +9,9 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators =[DataRequired(), EqualTo('password')] )
     submit = SubmitField('Sign Up')
 
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators =[DataRequired()] )
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
 
