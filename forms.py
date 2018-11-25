2576d228adff7dc750414150fb4f6dda6875f6c4
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-25 22:12:04 +0300

    created registration form for sign up

--- forms.py
+++ forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import Required
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Required,DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[])
+    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators =[DataRequired()] )
+    confirm_password = PasswordField('Confirm Password', validators =[DataRequired(), EqualTo('password')] )
+    submit = SubmitField('Sign Up')
+
+
