70dc8ec5c831b088c25133a48c13f2c8715e672d
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-25 21:51:57 +0300

    added flaskwtf validators

--- forms.py
+++ forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import Required
 
 class RegistrationForm(FlaskForm):
-    
+    username = StringField('Username', validators=[])
