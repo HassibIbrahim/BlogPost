0976ec6066c9771e86015a8a8211a0a2f7961c06
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-27 18:39:19 +0300

    added new post format

--- blog/routes.py
+++ blog/routes.py
@@ -75,14 +75,15 @@ def logout():
 
 
 def save_picture(form_picture):
-    random_hex=secrets.token_hex(8)
+    random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.route_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(
+        app.route_path, 'static/profile_pics', picture_fn)
 
-    output_size = (150, 150) 
+    output_size = (150, 150)
     i = Image.open(form_picture)
-    i.thumbnail(output_size)  
+    i.thumbnail(output_size)
     i.save(picture_path)
 
     return picture_fn
@@ -104,14 +105,17 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', 
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
-
 
 
 @app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
+    if form.validate_on_submit():
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home')) 
     return render_template('create_post.html', title='New Post', form=form)
